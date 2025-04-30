55adb2b08c7d69c130df921f88fc3168dc3f1703
sapunpramuditha <sapunpramuditha14@gmail.com>
2025-04-30 16:22:23 +0530

    Add product routes

---
+++ backend/routes/ProductRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/ProductController');
+const upload = require('../config/multer');
+
+
+router.post('/', upload.single('productImage'), productController.createProduct);
+router.get('/', productController.getProducts);
+router.put('/:id', upload.single('productImage'), productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+module.exports = router;  
