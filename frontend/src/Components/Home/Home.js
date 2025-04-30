6dbd8c0f3de74b8d4335678ae5a5c7480c0a3521
sapunpramuditha <sapunpramuditha14@gmail.com>
2025-04-30 19:38:19 +0530

    add home

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from "../Nav/Nav";
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+      <Nav/>
+     
+      
+      
+      
+    </div>
+  )
+}
+
+export default Home
